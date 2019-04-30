d6167bade0e0217f27dde3655e3f250b5688cea2
Jean Snyman <jean_snyman@hotmail.com>
2019-04-30 15:45:46 +0200

    deferredInstall

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,4 +1,6 @@
 /* Registering the service worker if it's available  */
+var deferredPrompt;
+
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
         .register('/sw.js', { scope: '/' })
@@ -6,3 +8,10 @@ if ('serviceWorker' in navigator) {
             console.log('Service woker registered');
         })
 }
+
+window.addEventListener('beforeinstallprompt', function (event) {
+    console.log('beforeinstallprompt fired');
+    event.preventDefault();
+    deferredPrompt = event;
+    return false;
+})
