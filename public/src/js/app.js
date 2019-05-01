f73ba42009daf26865a25a34a75c004972480875
Jean <jean.snyman@gmial.com>
2019-05-01 06:21:19 +0200

    added static caching

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,21 +1,24 @@
-/* Registering the service worker if it's available  */
+
 var deferredPrompt;
 
 if (!window.Promise) {
-    window.Promise = Promise;
+  window.Promise = Promise;
 }
 
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-        .register('/sw.js', { scope: '/' })
-        .then(function () {
-            console.log('Service woker registered');
-        })
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(function () {
+      console.log('Service worker registered!');
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
 }
 
-window.addEventListener('beforeinstallprompt', function (event) {
-    console.log('beforeinstallprompt fired');
-    event.preventDefault();
-    deferredPrompt = event;
-    return false;
-})
+window.addEventListener('beforeinstallprompt', function(event) {
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+});
