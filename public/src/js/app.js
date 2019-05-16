b9ec922d849a33b935d98124a1eb2690ae4a8452
Jean <jean.snyman@gmial.com>
2019-05-16 05:48:33 +0200

    added basics of notifications

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,23 +1,67 @@
 var deferredPrompt;
+var enableNotificationsButtons = document.querySelectorAll(
+  ".enable-notifications"
+);
 
 if (!window.Promise) {
   window.Promise = Promise;
 }
 
-if ('serviceWorker' in navigator) {
+if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .register('/sw.js')
+    .register("/sw.js")
     .then(function () {
-      console.log('Service worker registered!');
+      console.log("Service worker registered!");
     })
     .catch(function (err) {
       console.log(err);
     });
 }
-/* 
-window.addEventListener('beforeinstallprompt', function (event) {
-  console.log('beforeinstallprompt fired');
+
+window.addEventListener("beforeinstallprompt", function (event) {
+  console.log("beforeinstallprompt fired");
   event.preventDefault();
   deferredPrompt = event;
   return false;
-}); */
+});
+
+function displayConfirmNotification() {
+  if ('serviceWorker' in navigator) {
+    var options = {
+      body: 'You successfully subscribed to this service.',
+      icon: '/src/images/icons/app-icon-96x96.png',
+      image: '/src/images/sf-boat.jpg',
+      dir: 'ltr',
+      lang: 'en-US',
+      vibrate: [100, 50, 200],
+      badge: '/src/images/icons/app-icon-96x96.png'
+    };
+    navigator.serviceWorker.ready
+      .then(function (swreg) {
+        swreg.showNotification('Successfully subscribed! (From SW)', options);
+      })
+  }
+}
+
+function askForNotificationPermission() {
+  Notification.requestPermission(function (result) {
+    console.log("User Choice", result);
+    if (result !== "granted") {
+      console.log("No notification permission granted.");
+    } else {
+      displayConfirmNotification();
+    }
+  });
+}
+
+if ("Notification" in window) {
+  console.log("Yes, Notifications in available in the browser.");
+  for (var i = 0; i < enableNotificationsButtons.length; i++) {
+    enableNotificationsButtons[i].style.display =
+      "inline-block";
+    enableNotificationsButtons[i].addEventListener(
+      "click",
+      askForNotificationPermission
+    );
+  }
+}
