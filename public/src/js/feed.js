a4f146c77fefcff795b721d3e6981c3105d612f6
Jean <jean.snyman@gmial.com>
2019-05-21 05:23:15 +0200

    partially added camera features

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -5,11 +5,51 @@ var sharedMomentsArea = document.querySelector('#shared-moments');
 var form = document.querySelector('form');
 var titleInput = document.querySelector('#title');
 var locationInput = document.querySelector('#location');
+var videoPlayer = document.querySelector('#palyer');
+var canvasElement = document.querySelector('#canvas');
+var captureButton = document.querySelector('#capture-btn');
+var imagePicker = document.querySelector('#image-picker');
+var imagePickerArea = document.querySelector('#pick-image');
+
+function initializeMedia() {
+  if (!'mediaDevices' in navigator) {
+    if (!('mediaDevices' in navigator)) {
+      navigator.mediaDevices = {};
+    }
+
+    if (!('getUserMedia' in navigator.mediaDevices)) {
+      navigator.mediaDevices.getUserMedia = function (constraints) {
+        var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+        if (!getUserMedia) {
+          return Promise.reject(new Error('getUserMedia is not implemented'));
+        }
+
+        return new Promise(function (resolve, reject) {
+          getUserMedia.call(navigator, constraints, resolve, reject);
+        });
+      }
+    }
+  }
+
+  navigator.mediaDevices.getUserMedia({
+      video: true
+    })
+    .then(function (stream) {
+      videoPlayer.srcObject = stream;
+      videoPlayer.style.display = 'block';
+    })
+    .catch(function (err) {
+      imagePickerArea.style.display = 'block';
+      console.log(err);
+    })
+}
 
 function openCreatePostModal() {
   // createPostArea.style.display = 'block';
   // setTimeout(function() {
   createPostArea.style.transform = 'translateY(0)';
+  initializeMedia();
   // }, 1);
   if (deferredPrompt) {
     deferredPrompt.prompt();
@@ -39,7 +79,9 @@ function openCreatePostModal() {
 
 function closeCreatePostModal() {
   createPostArea.style.transform = 'translateY(100vh)';
-  // createPostArea.style.display = 'none';
+  imagePickerArea.style.display = 'none';
+  videoPlayer.style.display = 'none';
+  canvasElement.style.display = 'none';
 }
 
 shareImageButton.addEventListener('click', openCreatePostModal);
--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,8 @@
 importScripts('/src/js/idb.js');
 importScripts('/src/js/utility.js');
 
-var CACHE_STATIC_NAME = 'static-v9'
-var CACHE_DYNAMIC_NAME = 'dynamic-v9'
+var CACHE_STATIC_NAME = 'static-v14'
+var CACHE_DYNAMIC_NAME = 'dynamic-v14'
 var STATIC_FILES = [
   '/',
   '/index.html',
