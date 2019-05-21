6e5fddeb0c2ba27ec8bef9ef25e8d790ea7cee14
Jean <jean.snyman@gmial.com>
2019-05-21 05:36:10 +0200

    commit

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,8 @@
 importScripts('/src/js/idb.js');
 importScripts('/src/js/utility.js');
 
-var CACHE_STATIC_NAME = 'static-v15'
-var CACHE_DYNAMIC_NAME = 'dynamic-v15'
+var CACHE_STATIC_NAME = 'static-v16'
+var CACHE_DYNAMIC_NAME = 'dynamic-v16'
 var STATIC_FILES = [
   '/',
   '/index.html',
