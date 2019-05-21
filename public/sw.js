33fdc44bbd2e42bccff13313805a8dadba5b8fba
Jean <jean.snyman@gmial.com>
2019-05-21 05:39:33 +0200

    fixed video player

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,8 @@
 importScripts('/src/js/idb.js');
 importScripts('/src/js/utility.js');
 
-var CACHE_STATIC_NAME = 'static-v16'
-var CACHE_DYNAMIC_NAME = 'dynamic-v16'
+var CACHE_STATIC_NAME = 'static-v17'
+var CACHE_DYNAMIC_NAME = 'dynamic-v17'
 var STATIC_FILES = [
   '/',
   '/index.html',
